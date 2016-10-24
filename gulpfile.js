6242ac41700a119f636960a8f873688e10991c01
Whoin Lee <WhoINsAIR@Whoins-MacBook-Air.local>
2016-10-23 21:02:00 -0400

    Concatenated javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 //-- 'log' task, $gulp log
 // gulp.task('log', function() {
 // 	gutil.log('Workflows are awesome');
 // });
 
-//-- 'coffee' task
-var coffeeSources = ['components/coffee/tagline.coffee'];
 
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+];
+//-- 'coffee' task
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 	.pipe(coffee({ bare: true })
 		.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
+});
+//-- 'coffee' task
+gulp.task('js', function() {
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
