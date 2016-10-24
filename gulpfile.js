24d93946429dace0710436e0d49a06a652084972
Whoin Lee <WhoINsAIR@Whoins-MacBook-Air.local>
2016-10-23 20:49:29 -0400

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-//-- log task, $gulp log
-gulp.task('log', function() {
-	gutil.log('Workflows are awesome');
+//-- 'log' task, $gulp log
+// gulp.task('log', function() {
+// 	gutil.log('Workflows are awesome');
+// });
+
+//-- 'coffee' task
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+	.pipe(coffee({ bare: true })
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
 });
